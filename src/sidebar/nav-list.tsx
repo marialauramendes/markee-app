4e126ffddf783e7c2fee7724885fe6245805e5d8
Maria Laura <marialaurasmc@icloud.com>
2021-09-17 19:33:39 -0300

    add componente Status, ajustes estilização e tipagem ListItem

--- src/sidebar/nav-list.tsx
+++ src/sidebar/nav-list.tsx
@@ -1,11 +1,12 @@
-import styled from 'styled-components/macro'
+import { FileIcon, EditingIcon, SavingIcon, SavedIcon, DeleteIcon } from './icons'
+import { List, ListItem, Link, Status, DeleteButton } from './nav-styles'
 
 type archivesProps = {
   id: string
   name: string
   content: string
   active: boolean
-  status: 'editing' | 'saving' | 'saved'
+  status: 'editing' | 'saving' | 'saved',
 }
 
 const archives: archivesProps[] = [
@@ -17,8 +18,8 @@ const archives: archivesProps[] = [
     status: 'saved',
   },
   {
-    id: 'CONTRIBUTING.md',
-    name: 'CONTRIBUTING.md',
+    id: 'CONTRIBUT.md',
+    name: 'CONTRIBUT.md',
     content: '',
     active: false,
     status: 'saved',
@@ -51,96 +52,22 @@ function Navigation () {
     <nav>
       <List>
         {archives.map((item) => (
-          <ListItem key={item.id}>
+          <ListItem key={item.id} active={item.active}>
             <Link href={item.id}>
-              <svg width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M14 2.88062H6C5.46957 2.88062 4.96086 3.09133 4.58579 3.4664C4.21071 3.84147 4 4.35018 4 4.88062V20.8806C4 21.411 4.21071 21.9198 4.58579 22.2948C4.96086 22.6699 5.46957 22.8806 6 22.8806H18C18.5304 22.8806 19.0391 22.6699 19.4142 22.2948C19.7893 21.9198 20 21.411 20 20.8806V8.88062L14 2.88062Z' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M14 2.88062V8.88062H20' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 13.8806H8' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M16 17.8806H8' strokeLinecap='round' strokeLinejoin='round' />
-                <path d='M10 9.88062H9H8' strokeLinecap='round' strokeLinejoin='round' />
-              </svg>
+              <FileIcon />
               {item.name}
             </Link>
-            <DeleteButton>
-              <svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                <path d='M1.5 0.880615L9.18 8.56061M9.5 0.880615L1.82 8.56061' stroke='#fff' strokeWidth='1.5' strokeLinecap='round' />
-              </svg>
-            </DeleteButton>
+            <Status>
+              {item.active && item.status === 'editing' && <EditingIcon />}
+              {item.active && item.status === 'saving' && <SavingIcon />}
+              {item.active && item.status === 'saved' && <SavedIcon />}
+            </Status>
+            {item.active === false && <DeleteButton><DeleteIcon /></DeleteButton>}
           </ListItem>
         ))},
       </List>
     </nav>
   )
 }
-
-const List = styled.ul`
-  width:100%;
-  list-style-type: none;
-  padding-inline-start: 0;
-`
-
-const ListItem = styled.li`
-  display:flex;
-  align-items:center;
-  justify-content:space-between;
-  width:100%;
-  font-size: 1.6rem;
-  line-height: 2.1rem;
-  letter-spacing: -0.02em;
-  border-radius: 6px;
-  padding: 9px 14px 8px 14px;
-  -webkit-transition:.3s;
-  transition:.3s;
-
-  &:hover{
-    background-color:rgba(255,255,255, .05);
-    -webkit-transition:.3s;
-    transition:.3s;
-    a{
-    opacity:1;
-      svg{
-      stroke-opacity:1;
-      }
-    }
-    & button{
-      display:block;
-    }
-  }
-
-  &:active{
-    background-color:rgba(255,255,255, .05);
-    a{
-      opacity:1;
-    }
-    svg{
-      stroke: ${props => props.theme.colors.primary}
-    }
-  }
-
-`
-
-const Link = styled.a`
-  display: flex;
-  align-items:center;
-  font-weight: normal;
-  color:#fff;
-  opacity:.65;
-  text-decoration: none;
-  -webkit-transition:.3s;
-  transition:.3s;
-
-  & svg{
-    stroke: #fff;
-    stroke-opacity:0.65;
-    margin-right:20px;
-  }
-`
-
-const DeleteButton = styled.button`
-  display: none;
-  background-color:transparent;
-  border:none;
-`
 
 export { Navigation }
