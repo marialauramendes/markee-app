b8e9159c13f7eb2fb9061de1e82d28d5c1c86e9a
Maria Laura <marialaurasmc@icloud.com>
2021-09-22 12:53:35 -0300

    removido ternário desnecessário no map do array archives no componente navigation

--- src/sidebar/nav-list.tsx
+++ src/sidebar/nav-list.tsx
@@ -11,22 +11,20 @@ function Navigation ({ archives }: NavProps) {
   return (
     <nav>
       <List>
-        {archives
-          ? archives.map((item) => (
-            <ListItem key={`file/${item.id}`} active={item.active}>
-              <Link href={item.id}>
-                <FileIcon />
-                {item.name}
-              </Link>
-              <Status>
-                {item.active && item.status === 'editing' && <Editing />}
-                {item.active && item.status === 'saving' && <Loading />}
-                {item.active && item.status === 'saved' && <Saved />}
-              </Status>
-              {item.active === false && <DeleteButton><Delete /></DeleteButton>}
-            </ListItem>
-          ))
-          : <ListItem key={`file/${'1'}`} active={false}>teste</ListItem>},
+        {archives.map((item) => (
+          <ListItem key={`file/${item.id}`} active={item.active}>
+            <Link href={item.id}>
+              <FileIcon />
+              {item.name}
+            </Link>
+            <Status>
+              {item.active && item.status === 'editing' && <Editing />}
+              {item.active && item.status === 'saving' && <Loading />}
+              {item.active && item.status === 'saved' && <Saved />}
+            </Status>
+            {item.active === false && <DeleteButton><Delete /></DeleteButton>}
+          </ListItem>
+        ))},
       </List>
     </nav>
   )
