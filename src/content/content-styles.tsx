78e0b29b856be6eb513b6355164fdb1f756679aa
Maria Laura <marialaurasmc@icloud.com>
2021-09-25 12:48:40 -0300

    adicionado autoFocus ao input e estilos ao Output

--- src/content/content-styles.tsx
+++ src/content/content-styles.tsx
@@ -77,6 +77,10 @@ width:50%;
 height:100%;
 padding: 0 24px 24px;
 background-color:transparent;
+font-size: 1.8rem;
+line-height: 2.3rem;
+font-weight: 400;
+color: ${props => props.theme.colors.black};
 
 h1, h2, h3, h4, h5, h6{
   margin-top: 0;
@@ -92,10 +96,6 @@ h2{
 }
 
 p{
-  font-size: 1.8rem;
-  line-height: 2.3rem;
-  font-weight: 400;
-  color: ${props => props.theme.colors.black};
   opacity:.70;
 }
 `
